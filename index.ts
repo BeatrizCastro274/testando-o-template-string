cc1007536a98ec390ee5c227a03abdbeac14b6c0
BeatrizCastro274 <biawolf4@hotmail.com>
2022-08-19 20:09:35 -0300

    contador por segundos

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 let title: string = 'MyApp';
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>${title}</h1>
-<hr/>
-<p>Parágrafo qualquer...</p>
-`;
+const appDiv: HTMLElement = document.getElementById('app.title');
 
-console.log('Olá, mundo!');
+let counter = 0;
 
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
-  }
-}
+let intervalId = setInterval(() => {
+  counter = counter + 1; //counter++
+  let counterDiv = document.getElementById('app.counter');
+  counterDiv.innerHTML = counter.toString();
+}, 1000);
+
+//appDiv.innerHTML = `<h1>${title}</h1>
+//<hr/>
+//<p>Parágrafo qualquer...</p>
+//`;
+
+//console.log('Olá, mundo!');
+
+//for (let i = 1; i <= 10; i++) {
+//  if (i % 2 == 0) {
+//    console.log(i);
+//  }
+//}
