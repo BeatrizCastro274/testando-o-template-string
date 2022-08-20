c2a0eb86211b29172022f0783a5a501de3841a26
BeatrizCastro274 <biawolf4@hotmail.com>
2022-08-19 21:24:06 -0300

    contador de segundos

--- index.ts
+++ index.ts
@@ -1,12 +1,26 @@
-let title: string = 'MyApp';
+let title: string = 'Tabata Timer';
 const appDiv: HTMLElement = document.getElementById('app.title');
 
 let counter = 0;
+let rodada = 0;
+let estado = 'Relaxado';
 
 let intervalId = setInterval(() => {
   counter = counter + 1; //counter++
   let counterDiv = document.getElementById('app.counter');
+  let rodadaDiv = document.getElementById('app.rodada');
+  let estadoDiv = document.getElementById('app.estado');
+  if (rodada < 8) {
+    if (counter < 10) {
+    }
+    if (counter == 30) {
+      counter = 0;
+      rodada++;
+    }
+  }
+  estadoDiv.innerHTML = estado;
   counterDiv.innerHTML = counter.toString();
+  rodadaDiv.innerHTML = rodada.toString();
 }, 1000);
 
 //appDiv.innerHTML = `<h1>${title}</h1>
